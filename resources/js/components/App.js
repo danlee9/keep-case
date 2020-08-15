6beadaff013e9b66a7ae7c63a2623003fdf241ae
Daniel Lee <dan.dh.lee@gmail.com>
2020-08-15 01:16:59 -0700

    Initial boilerplate

---
+++ resources/js/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Router, Route, Switch } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                Blah
+            </div>
+        );
+    }
+}
+
+export default App;
