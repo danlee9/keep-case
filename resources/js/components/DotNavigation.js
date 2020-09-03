ba65c8c267b85a71e22cd610381ce887dbef050f
Daniel Lee <dan.dh.lee@gmail.com>
2020-09-03 01:55:38 -0700

    Add dot navigation component

--- resources/js/components/DotNavigation.js
+++ resources/js/components/DotNavigation.js
@@ -1,2 +1,22 @@
 import React from 'react';
 
+import './DotNavigation.css';
+
+class DotNavigation extends React.Component {
+    
+
+    render() {
+        return (
+            <div id="dot-nav">
+                <ul>
+                    <li className="awesome-tooltip active" title="Home"><a href="#home"></a></li>
+                    <li className="awesome-tooltip" title="About"><a href="#about"></a></li>
+                    <li className="awesome-tooltip" title="Projects"><a href="#projects"></a></li>
+                    <li className="awesome-tooltip" title="Contact"><a href="#contact"></a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default DotNavigation;
