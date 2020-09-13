51b43a3399c13ecf2e66ca67b1d02cef89e47a49
Daniel Lee <dan.dh.lee@gmail.com>
2020-09-13 02:32:07 -0700

    Add swiper package

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -12,7 +12,7 @@ import KeepCaseContent from './KeepCaseContent';
 import BuyKeepCase from './BuyKeepCase';
 import DotNavigation from "./DotNavigation";
 
-function smoothScroll(offset) {
+function smoothScroll(offset, direction) {
     console.log('offset')
     window.scrollTo({
         top: offset,
@@ -21,29 +21,34 @@ function smoothScroll(offset) {
     
     return new Promise((resolve, reject) => {
         const failed = setTimeout(() => {
-            if (Math.round(window.pageYOffset) === offset) {
-                window.removeEventListener("scroll", scrollHandler);
-                console.log('timeout resolved')
-                resolve();
-            } else {
-                reject();
-            }
+            console.log('rejected');
+            reject();
         }, 2000);
   
         const scrollHandler = () => {
             console.log(offset);
             console.log(window.pageYOffset);
-            if (window.pageYOffset === offset) {
-                window.removeEventListener("scroll", scrollHandler);
-                clearTimeout(failed);
-                console.log('first resolved')
-                resolve();
+            if (direction === 'ArrowDown') {
+                if (window.pageYOffset >= offset) {
+                    window.removeEventListener("scroll", scrollHandler);
+                    clearTimeout(failed);
+                    console.log('resolved while scrolling down')
+                    resolve();
+                }
+            } else {
+                if (window.pageYOffset <= offset) {
+                    window.removeEventListener("scroll", scrollHandler);
+                    clearTimeout(failed);
+                    console.log('resolved while scrolling up')
+                    resolve();
+                }
             }
+            
         };
 
         if (Math.round(window.pageYOffset) === offset) {
             clearTimeout(failed);
-            console.log('second resolved resolved')
+            console.log('resolved without adding event listener')
             resolve();
         } else {
             console.log('add scroll listener')
@@ -66,100 +71,142 @@ class App extends React.Component {
 
     componentWillMount() {
         window.addEventListener('keydown', this.arrowKeyPress);
+        window.addEventListener('wheel', this.wheelScroll, {passive: false});
         // window.addEventListener('scroll', this.mouseScroll);
     }
 
     componentWillUnmount() {
         window.removeEventListener('keydown', this.arrowKeyPress);
+        window.removeEventListener('wheel', this.wheelScroll);
         // window.removeEventListener('scroll', this.mouseScroll);
+    }
+
+    scrollSection = direction => {
+        const { sections, position } = this.props;
+        if (direction == 'ArrowUp') {
+            if (position != 0) {
+                scroll({
+                    top: sections[position - 1].offset,
+                    behavior: 'smooth'
+                });
+            }
+        } else {
+            if (position != sections.length - 1) {
+                scroll({
+                    top: sections[position + 1].offset,
+                    behavior: 'smooth'
+                });
+            }
+        }
+        this.props.arrowKeyPress(direction);
+    }
+
+    wheelScroll = e => {
+        console.log('wheel');
+        e.preventDefault();
+        console.log(e.deltaY);
+        const { sections, position } = this.props;
+        var direction;
+        // e.deltaY > 0 ? direction = 'ArrowDown' : direction = 'ArrowUp';
+        if (e.deltaY > 0) {
+            direction = 'ArrowDown';
+        } else if (e.deltaY < 0) {
+            direction = 'ArrowUp';
+        }
+        this.scrollSection(direction);
+        // if (direction == 'ArrowUp') {
+        //     if (position != 0) {
+        //         scroll({
+        //             top: sections[position - 1].offset,
+        //             behavior: 'smooth'
+        //         });
+        //     }
+        // } else {
+        //     if (position != sections.length - 1) {
+        //         scroll({
+        //             top: sections[position + 1].offset,
+        //             behavior: 'smooth'
+        //         });
+        //     }
+        // }
+        // this.props.arrowKeyPress(direction);
     }
 
     arrowKeyPress = e => {
         if (e.key == 'ArrowDown' || e.key == 'ArrowUp') {
             e.preventDefault();
             const direction = e.key;
-            const { sections, position } = this.props;
-            if (direction == 'ArrowUp') {
-                if (position != 0) {
-                    scroll({
-                        top: sections[position - 1].offset,
-                        behavior: 'smooth'
-                    });
-                }
-            } else {
-                if (position != sections.length - 1) {
-                    scroll({
-                        top: sections[position + 1].offset,
-                        behavior: 'smooth'
-                    });
-                }
-            }
-            this.props.arrowKeyPress(direction);
+            this.scrollSection(direction);
+            // const { sections, position } = this.props;
+            // if (direction == 'ArrowUp') {
+            //     if (position != 0) {
+            //         scroll({
+            //             top: sections[position - 1].offset,
+            //             behavior: 'smooth'
+            //         });
+            //     }
+            // } else {
+            //     if (position != sections.length - 1) {
+            //         scroll({
+            //             top: sections[position + 1].offset,
+            //             behavior: 'smooth'
+            //         });
+            //     }
+            // }
+            // this.props.arrowKeyPress(direction);
         }
     }
 
-    mouseScroll = e => {
-        // console.log(e);
-        // e.preventDefault();
-        if (this.state.scrolling) {
-            console.log('state scrolling');
-            return;
-        }
-        const { sections, position } = this.props;        
+    // mouseScroll = e => {
+    //     // console.log(e);
+    //     // e.preventDefault();
+    //     if (this.state.scrolling) {
+    //         console.log('state scrolling');
+    //         return;
+    //     }
+    //     const { sections, position } = this.props;        
         
-        let newScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (newScrollTop > this.state.offset) {
-            console.log('scrolldown')
-            let direction = 'ArrowDown';
-            if (position != sections.length - 1) {
-                this.setState({
-                    scrolling: true,
-                    scrollDirection: direction
-                });
-                // scroll({
-                //     top: sections[position + 1].offset,
-                //     behavior: 'smooth'
-                // });
-                // this.setState({
-                //     scrolling: false,
-                //     offset: sections[position + 1].offset
-                // });
-                let p = smoothScroll(sections[position + 1].offset)
-                p.then(() => {
-                    this.setState({
-                        scrolling: false,
-                        offset: sections[position + 1].offset
-                    });
-                    this.props.arrowKeyPress(this.state.scrollDirection);
-                })
-            }
+    //     let newScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    //     if (newScrollTop > this.state.offset) {
+    //         console.log('scrolldown')
+    //         let direction = 'ArrowDown';
+    //         if (position != sections.length - 1) {
+    //             this.setState({
+    //                 scrolling: true,
+    //                 scrollDirection: direction
+    //             });
+    //             let p = smoothScroll(sections[position + 1].offset, direction)
+    //             p.then(() => {
+    //                 this.setState({
+    //                     scrolling: false,
+    //                     offset: sections[position + 1].offset
+    //                 });
+    //                 this.props.arrowKeyPress(this.state.scrollDirection);
+    //             })
+    //         }
             
-            // this.props.arrowKeyPress(direction);
-        } else {
-            console.log('scrollup');
-            let direction = 'ArrowUp';
-            if (position != 0) {
-                // scroll({
-                //     top: sections[position - 1].offset,
-                //     behavior: 'smooth'
-                // });
-                this.setState({
-                    scrolling: true,
-                    scrollingDirection: direction
-                });
-                let p = smoothScroll(sections[position - 1].offset)
-                p.then(() => {
-                    this.setState({
-                        scrolling: false,
-                        offset: sections[position - 1].offset
-                    });
-                    this.props.arrowKeyPress(this.state.scrollDirection);
-                })
-            }
+    //         // this.props.arrowKeyPress(direction);
+    //     } else {
+    //         console.log('scrollup');
+    //         let direction = 'ArrowUp';
+    //         if (position != 0) {
+    //             this.setState({
+    //                 scrolling: true,
+    //                 scrollingDirection: direction
+    //             });
+    //             let p = smoothScroll(sections[position - 1].offset, direction)
+    //             p.then(() => {
+    //                 this.setState({
+    //                     scrolling: false,
+    //                     offset: sections[position - 1].offset
+    //                 });
+    //                 this.props.arrowKeyPress(this.state.scrollDirection);
+    //             })
+    //         }
             
-            // this.props.arrowKeyPress(direction);
-        }
-    }
+    //         // this.props.arrowKeyPress(direction);
+    //     }
+    // }
 
     render() {
         return (
